372b31c33792ff6b7f82bc0d1317fff92f42fe15
Vlad <vlad.miryaha@gmail.com>
2018-03-09 21:39:37 +0300

    fixit

--- WordFreq.cpp
+++ WordFreq.cpp
@@ -8,7 +8,24 @@
 #include <string>
 #include <cctype>
 //#include <cccatch>
+
+/*
+fixit:
+вы все равно везде пишете префикс std::
+давайте уберем строку
 using namespace std;
+*/
+using namespace std;
+
+/*
+fixit: не усложняйте то, что можно сделать просто:
+
+using Pair = std::pair<std::string, int>;
+bool compare(const Pair& lhs, const Pair& rhs)
+{
+	return lhs.second >= rhs.second;
+}
+*/
 typedef std::function<bool(std::pair<std::string, int>, std::pair<std::string, int>)> Comparator;
  
 	// Defining a  function to compare two pairs. It will compare two pairs using second field
@@ -17,6 +34,14 @@ Comparator compFunctor =
 			{
 				return elem1.second >= elem2.second;
 			};
+
+
+/*
+fixit:
+из названия неясно, что делает ф-я.
+что значить "подготовить стандартный поток ввода"?
+чем не понравился вариант из презентации?
+*/
 std::string prepare(std::istream &in)
 {
     char c;
@@ -42,6 +67,9 @@ int main() {
 	std::map<std::string, int> mapOfWordCount;
 	std::ifstream fin("input.txt");
     	std::string s;
+	/*
+	зачем отдельная переменная под пустую строку?
+	*/
     	std::string empty ="";
     	while((s=prepare(fin))!=empty )
             ++mapOfWordCount[s]; 
@@ -49,6 +77,12 @@ int main() {
 
  
 	// Declaring a set that will store the pairs using above comparision logic
+	/*
+	Я понял вашу задумку с компаратором только когда долистал до этого места.
+	Если что-то можно сделать просто, то лучше так и сделать.
+	Когда вы пойдете работать, от вас будут требовать максимально простой/понятный код, а не просто
+	работающий. Понятный код -> новый разработчик меньше тратит времени на вникание -> вы экономите деньги компании.
+	*/
 	std::set<std::pair<std::string, int>, Comparator> setOfWords(
 			mapOfWordCount.begin(), mapOfWordCount.end(), compFunctor);
 			//creating File
@@ -56,6 +90,10 @@ int main() {
 	outputFile.open ("output.txt");
 	// Iterate over a set using range base for loop
 	// It will display the items in sorted order of values
+	/*
+	может
+	for (const auto& element : setOfWords) ?
+	*/
 	for (std::pair<std::string, int> element : setOfWords){//possibly auto would be better 
 		std::cout << element.first << " - " << element.second << std::endl;
 		//Outputting overall frequency files 
